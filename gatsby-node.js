59bdd6dd015aa46df9804ef8ecd8555e84621203
sanhapon <sanhapon@yahoo.com>
2023-03-10 16:37:26 +0700

    try gzip

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,27 @@
-// const fs = require('fs');
-// const path = require('path');
-// const zlib = require('zlib');
-// const iltorb = require('iltorb');
-// const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+const iltorb = require('iltorb');
+const glob = require('glob');
 
 exports.onPostBuild = () =>
   new Promise((resolve, reject) => {
-    // try {
-    //   const publicPath = path.join(__dirname, 'public');
-    //   const gzippable = glob.sync(`${publicPath}/**/?(*.html|*.js|*.css|*.svg)`, { nodir: true });
-    //   gzippable.forEach((file) => {
-    //     const content = fs.readFileSync(file);
-    //     const zipped = zlib.gzipSync(content);
-    //     fs.writeFileSync(`${file}.gz`, zipped);
+    try {
+      const publicPath = path.join(__dirname, 'public');
+      const gzippable = glob.sync(`${publicPath}/**/?(*.html|*.js|*.css|*.svg)`, { nodir: true });
+      gzippable.forEach((file) => {
+        const content = fs.readFileSync(file);
+        const zipped = zlib.gzipSync(content);
+        // fs.writeFileSync(`${file}.gz`, zipped);
+        fs.writeFileSync(file, zipped);
         
-    //     // const brotlied = iltorb.compressSync(content);
-    //     // fs.writeFileSync(`${file}.br`, brotlied);
-    //   });
-    // } catch (e) {
-    //   console.log(e)
-    //   reject(new Error('onPostBuild: Could not compress the files:'));
-    // }
+        // const brotlied = iltorb.compressSync(content);
+        // fs.writeFileSync(`${file}.br`, brotlied);
+      });
+    } catch (e) {
+      console.log(e)
+      reject(new Error('onPostBuild: Could not compress the files:'));
+    }
 
     resolve();
   });
